0cb09cf06e01f2ddc7b00ac4d22ef41f8f775b69
Alhammanazil <alhammanazil18@gmail.com>
2025-06-30 16:15:40 +0700

    update: route /home

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// home
+Route::get('/home', function () {
+    return view('home');
+});
+
+// about
+Route::get('/about', function () {
+    return view('about');
+});
